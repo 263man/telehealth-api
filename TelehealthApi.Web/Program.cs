aec6ed8620a1d730ef0a5532c49b5cc9a5cc9c41
Keps <fckepekepe@gmail.com>
2025-07-08 20:34:09 +0200

    Enhanced connection string privacy

--- TelehealthApi.Web/Program.cs
+++ TelehealthApi.Web/Program.cs
@@ -2,9 +2,37 @@ using TelehealthApi.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// This line is what loads appsettings.json and then user secrets in development
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+// This line specifically adds user secrets when in Development environment
+builder.Configuration.AddUserSecrets<Program>(); // Or whatever your startup class is
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Register HttpClient with the correct BaseAddress
+builder.Services.AddHttpClient("PatientOnboard", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["BaseAddress"] ?? "https://localhost:7259");
+});
+
+// Configure HTTPS redirection to use the correct port
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+    options.HttpsPort = 7259;
+});
+
+// Optional: Configure HSTS for production
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(60);
+});
 
 var app = builder.Build();
 
@@ -12,13 +40,13 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
+
+// Only include antiforgery if necessary (see below)
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
